3c8b1f4b039eef09f97672440f28968deba37933
Wali <walisylla.esse@gmail.com>
2024-11-06 16:11:15 +0100

    cartjs

--- cart.js
+++ cart.js
@@ -1,17 +1,48 @@
+const travelDomElem = document.querySelector('#travel');
+
 fetch(`http://localhost:3000/carts`)
 .then(response => response.json())
 .then(data => {
-    console.log(data)
+    //affichage heure
+   
+    console.log(data.Cart[0].departure)
+    for (let i =0; i<data.Cart.length; i++) {
+        const dateString = data.Cart[i].date;
+        const dateObj = new Date(dateString);
+        const heure = dateObj.getHours();
+        const minute = dateObj.getMinutes() < 10 ? "0"+ dateObj.getMinutes() : dateObj.getMinutes();
+        let deleteCart = document.getElementById('deletCart');
+        console.log(data.Cart[i])       
+        travelDomElem.innerHTML += `
+        <div id="cart-list">
+            <span>${data.Cart[i].departure} > ${data.Cart[i].arrival}</span> <span>${heure} : ${minute}</span> <span id="spanPrice">${data.Cart[i].price}€</span>
+            <button id="${deleteCart}">X</button>
+        </div>
+    `
+    let totalPrice = 0
+    if (travelDomElem.length > 0) {
+        
+        travelDomElem.forEach(book => {
+        totalPrice += (book.price * book.length);
+        console.log(totalPrice)
+      });
+    } else {
+        console.log(totalPrice = book.price)
+    }
+    
+    }
+    
+    // console.log(data)
 })
 
 
 const totalPrice = () => {
-    spanTotalPrice
+    let TotalPrice
     if (products.length > 0) {
     let totalPrice = 0;
    
-    products.forEach(product => {
-      totalPrice += (product.price * product.quantity);
+    products.forEach(book => {
+      totalPrice += (book.price * book[i]);
     });
   
     spanTotalPrice.textContent = totalPrice + " €";
