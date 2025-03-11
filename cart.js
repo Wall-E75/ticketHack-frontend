d9df51ade850e93fe6ef9a19b08ffb9850fcb66f
Wali <walisylla.esse@gmail.com>
2025-03-11 15:51:07 +0100

    suppression des console.log des pages index et carts

--- cart.js
+++ cart.js
@@ -11,7 +11,6 @@ fetch(`https://ticket-hack-backend-five.vercel.app/carts`)
             return;
         }
         // affichage heure
-        console.log(data.Cart[0].departure);
         travelDomElem.innerHTML = '';
         let bookingIds = [];
 
@@ -20,7 +19,6 @@ fetch(`https://ticket-hack-backend-five.vercel.app/carts`)
             const dateObj = new Date(dateString);
             const heure = dateObj.getHours();
             const minute = dateObj.getMinutes() < 10 ? "0" + dateObj.getMinutes() : dateObj.getMinutes();
-            console.log("Data Cart =>", data.Cart);
 
             travelDomElem.innerHTML += `
             <div class="cart-list" data-id="${data.Cart[i]._id}">
@@ -31,15 +29,12 @@ fetch(`https://ticket-hack-backend-five.vercel.app/carts`)
             </div>
             `;
             bookingIds.push(data.Cart[i]._id);
-            console.log('IDS =>', bookingIds);
-            console.log('id =>', { id: data.Cart[i]._id });
         }
 
         totalCart();
         deleteButton();
 
         btnPurchase.addEventListener('click', () => {
-            console.log('click');
             if (bookingIds.length === 0) return alert("Votre panier est vide");
 
             fetch('https://ticket-hack-backend-five.vercel.app/bookings', {
@@ -57,7 +52,6 @@ fetch(`https://ticket-hack-backend-five.vercel.app/carts`)
                     })
                     .then(response => response.json())
                     .then(deleteData => {
-                        console.log('Data after purchase =>', deleteData);
                         // On vide le DOM seulement si la suppression est réussie
                         travelDomElem.textContent = '';
                         document.getElementById('total').textContent = 'Total : 0€';
@@ -100,10 +94,8 @@ function totalCart() {
     if (totalSpanPrice.length > 0) {
         for (let i=0; i<totalSpanPrice.length; i++) {
             price += Number(totalSpanPrice[i].textContent)
-            // console.log(price)
         }
         totalPrice = price;
-        console.log(totalPrice)
         document.querySelector('#total').textContent = 'Total : ' + totalPrice + '€'
     } 
    
--- script.js
+++ script.js
@@ -2,7 +2,6 @@
  const departInputElement = document.querySelector('#departure');
  const arrivalInputElement = document.querySelector('#arrival');
  const dateInputElement = document.querySelector('#date');
- console.log(departInputElement)
 
 document.querySelector('#btn-search').addEventListener('click', ()=> {
     //relation avec la partie affichage
@@ -21,19 +20,16 @@ document.querySelector('#btn-search').addEventListener('click', ()=> {
     fetch(`https://ticket-hack-backend-five.vercel.app/trips/${departInputElement.value}/${arrivalInputElement.value}/${dateInputElement.value}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data) 
         let trip = data.trip   
         if(data.result) {
             const defaultOutputElem = document.querySelector('#default-output');
             defaultOutputElem.style.display = "none";
             for (let i = 0; i < trip.length; i++) {
-                console.log(trip[i])
                 //Changement du type de la date (qui est en string) en objet date
                 const dateString = trip[i].date;
                 const dateObj = new Date(dateString);
                 const heure = dateObj.getHours();
                 const minute = dateObj.getMinutes() < 10 ? "0" + dateObj.getMinutes() : dateObj.getMinutes();
-                // console.log();
                 document.querySelector('#resa-output').innerHTML += `
                <div id="book-output">
                     <p>${trip[i].departure} > ${trip[i].arrival} ${heure}:${minute} ${trip[i].price}€</p>
@@ -53,7 +49,6 @@ document.querySelector('#btn-search').addEventListener('click', ()=> {
     //             <p>It's time to book your future trip</p>
     //         </div>
     //     `
-    // console.log(data);
      
 })
     searchReload();
@@ -65,16 +60,13 @@ document.querySelector('#btn-search').addEventListener('click', ()=> {
 function addBook() {
     for (let i =0; i < document.querySelectorAll('.btn-book').length; i++) {
         document.querySelectorAll('.btn-book')[i].addEventListener('click', function () {
-            console.log('click', this.id)
             fetch(`https://ticket-hack-backend-five.vercel.app/carts`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({id: this.id}),
             }).then(response => response.json())
                 .then(data => {
-                    console.log(data);
                     if (data) {
-                        console.log('La page change pour cart.html')
                         window.location.assign('./cart.html');
                         return
                     }
@@ -101,7 +93,6 @@ function searchReload() {
     fetch(`https://ticket-hack-backend-five.vercel.app/trips/${departInputElement.value}/${arrivalInputElement.value}/${dateInputElement.value}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data) 
         let trip = data.trip   
         if(data.result) {
             const defaultOutputElem = document.querySelector('#default-output');
@@ -112,7 +103,6 @@ function searchReload() {
                 const dateObj = new Date(dateString);
                 const heure = dateObj.getHours();
                 const minute = dateObj.getMinutes() < 10 ? "0" + dateObj.getMinutes() : dateObj.getMinutes();
-                // console.log();
                 document.querySelector('#resa-output').innerHTML += `
                <div id="book-output">
                     <p>${trip[i].departure} > ${trip[i].arrival} ${heure}:${minute} ${trip[i].price}€</p>
@@ -132,7 +122,6 @@ function searchReload() {
     //             <p>It's time to book your future trip</p>
     //         </div>
     //     `
-    // console.log(data);
      
 })
 }
